468e2a9e8198add09868149fe8fbce6c5547bc63
Lars Kellogg-Stedman <lars@oddbit.com>
2020-09-13 12:18:13 -0400

    add meal model

--- paprika/models.py
+++ paprika/models.py
@@ -9,12 +9,20 @@ class RecipeIndex(db.Entity):
     id = orm.PrimaryKey(int, auto=True)
     uid = orm.Required(str, unique=True, index=True)
     hash = orm.Required(str)
-    last_update = orm.Required(datetime,
-                               default=datetime.utcnow)
+    last_update = orm.Required(datetime, default=datetime.utcnow)
     recipe = orm.Optional('Recipe')
 
 
 class Recipe(db.Entity):
     id = orm.PrimaryKey(RecipeIndex)
     data = orm.Required(orm.Json)
-    name = orm.Required(str)
+
+
+class Meal(db.Entity):
+    id = orm.PrimaryKey(int, auto=True)
+    uid = orm.Required(str)
+    recipe_uid = orm.Required(str)
+    recipe_name = orm.Required(str)
+    meal_date = orm.Required(datetime)
+    meal_order = orm.Required(int)
+    meal_type = orm.Required(int)
